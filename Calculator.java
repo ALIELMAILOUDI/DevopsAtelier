67af2dd7a89c1d6170576a56a721077a301c61ea
med03ali <medali24122003@gmail.com>
2025-04-16 11:20:49 +0100

    merge

--- Calculator.java
+++ Calculator.java
@@ -8,5 +8,6 @@ public class Calculator {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("5 + 3 = " + calc.add(5, 3));
+        System.out.println("now we merge ");
     }
 }
