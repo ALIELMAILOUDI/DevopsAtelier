dd5311600698fb42faf451733a6826488c6fca03
med03ali <medali24122003@gmail.com>
2025-04-16 11:28:54 +0100

    commit2

--- Calculator.java
+++ Calculator.java
@@ -10,5 +10,7 @@ public class Calculator {
         System.out.println("5 + 3 = " + calc.add(5, 3));
         System.out.println("now we merge ");
         System.out.println("rebase_commit1 ");
+        System.out.println("rebase_commit2 ");
+
     }
 }
